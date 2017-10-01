afd3d2dc833275f6447d17455f23fa2563ee6f2b
Chinmay Awale <chinubeta0710@gmail.com>
2017-10-01 20:35:29 +0530

    minor server config

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var serv = require('http').Server(app);
 var ioHandle = require('socket.io').listen(serv);
 
 app.use('/js',exp.static(__dirname + '/client/js'));
+app.use('/phaser',exp.static(__dirname + '/client/phaser'));
+
 
 app.get('/',function (req, res) {
-	res.sendfile(__dirname + '/client/index.html');
+	res.sendFile(__dirname + '/client/index.html');
 })
 
 serv.listen('4040', function () {
