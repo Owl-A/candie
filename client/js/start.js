57643ffe461df65c005c237eab8386e26c3ce326
Chinmay Awale <chinubeta0710@gmail.com>
2017-10-26 04:06:55 +0530

    intro improved

--- client/js/start.js
+++ client/js/start.js
@@ -1,4 +1,4 @@
-widthTex = 600;
+// widthTex = 600;
 var Name;
 var start =function(game){
 };
@@ -10,28 +10,34 @@ start.prototype = {
 		 game.load.image('gfood', '/assets/gfood.png');
 		 game.load.image('bfood', '/assets/bfood.png');
 		 game.load.image('backdrop','/assets/backdrop.png');
+		 game.load.spritesheet('intro','/assets/intro_sheet.png',547,472,3);
 	},
 
 	create: function() {
 		game.add.plugin(PhaserInput.Plugin);
-		game.world.setBounds(0, 0, 3000, 3000); // bounds of the world
-		game.physics.startSystem(Phaser.Physics.P2JS);
-		game.stage.disableVisibilityChange = true;
-		game.physics.p2.setImpactEvents(true);
-		game.physics.p2.restitution = 1;
+		game.world.setBounds(0, 0, document.documentElement.clientWidth, document.documentElement.clientHeight); // bounds of the world
+		// game.physics.startSystem(Phaser.Physics.P2JS);
+		// game.stage.disableVisibilityChange = true;
+		// game.physics.p2.setImpactEvents(true);
+		// game.physics.p2.restitution = 1;
 		if (socket) {socket.close()};
 		socket = io.connect();
+		var intro = game.add.sprite(document.documentElement.clientWidth/2, 10, 'intro');
+		intro.anchor.set(0.5,0);
+		intro.scale.setTo(0.75*document.documentElement.clientHeight/intro.height);
+		intro.animations.add('flicker');
+		intro.animations.play('flicker',0.5,true);
 
 		game.stage.backgroundColor = 0xc0aeef;
-		game.add.tileSprite(0,0,3000,3000,'backdrop');
+		// game.add.tileSprite(0,0,3000,3000,'backdrop');
 		var wkey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
 		wkey.onDown.add(this.star, this);
 
-		Name = game.add.inputField(document.documentElement.clientWidth/2 - widthTex/2, document.documentElement.clientHeight/2 - 10,  {
+		Name = game.add.inputField(document.documentElement.clientWidth/2 - intro.width/2, document.documentElement.clientHeight*(0.82) ,  {
 		    font: '35px Arial',
 		    fill: '#212121',
 		    fontWeight: 'bold',
-		    width: widthTex,
+		    width: intro.width,
 		    padding: 8,
 		    borderWidth: 4,
 		    borderColor: '#000',
